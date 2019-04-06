bf37f1def003a5560bf58d4cd46c4bce3dae83c4
VladimirMundl <vladimir.mundl@gmail.com>
2019-04-06 21:49:45 +0200

    notes form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,46 @@
 import * as React from "react";
 
-export class App extends React.Component<Iprops, {}> {
-    render() {
-        return <h1>Hello {this.props.name}</h1>;
-    }
+export class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      currentNote: "",
+      notes: []
+    };
+  }
+
+  handleSubmit(e: any) {
+    e.preventDefault();
+    this.setState({
+      currentNote: "",
+      notes: [
+        ...this.state.notes,
+        this.state.currentNote
+      ]
+    })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <h1>Notes application</h1>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="add something"
+            value={this.state.currentNote}
+            onChange={e => this.setState({ currentNote: e.target.value })}
+          />
+          <button type="submit">add note</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-interface Iprops {
-    name: string;
+interface IState {
+  currentNote: string;
+  notes: Array<string>;
 }
