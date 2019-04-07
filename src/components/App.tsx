71379ecf3c13e8de6ed433cee5be1625e9865862
VladimirMundl <vladimir.mundl@gmail.com>
2019-04-07 11:06:01 +0200

    note delete

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { number } from "prop-types";
 
 export class App extends React.Component<{}, IState> {
   constructor(props: {}) {
@@ -16,20 +17,32 @@ export class App extends React.Component<{}, IState> {
       currentNote: "",
       notes: [
         ...this.state.notes,
-        this.state.currentNote
+        {
+          id: this._timeInMilliseconds(),
+          value: this.state.currentNote
+        }
       ]
-    })
+    });
   }
 
-
-
+  public deleteNote(id: number): void {
+    const filteredNotes: Array<INote> = this.state.notes.filter((note: INote) => note.id !== id);
+    this.setState({notes: filteredNotes})
+  }
 
   public renderNotes(): JSX.Element[] {
-    return this.state.notes.map((note: string, index: number) => <div key={index}>{note}</div>);
-  };
+    return this.state.notes.map((note: INote, index: number) => {
+      return (
+        <div key={note.id}>
+          <span>{note.value}</span>
+          <button onClick={() => this.deleteNote(note.id)}>Delete</button>
+        </div>
+      );
+    });
+  }
 
   public render(): JSX.Element {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <div>
         <h1>Notes application</h1>
@@ -42,13 +55,23 @@ export class App extends React.Component<{}, IState> {
           />
           <button type="submit">add note</button>
         </form>
-        <section>{ this.renderNotes() }</section>
+        <section>{this.renderNotes()}</section>
       </div>
     );
+  }
+
+  private _timeInMilliseconds(): number {
+    const date: Date = new Date();
+    return date.getTime();
   }
 }
 
 interface IState {
   currentNote: string;
-  notes: Array<string>;
+  notes: Array<INote>;
+}
+
+interface INote {
+  id: number;
+  value: string;
 }
